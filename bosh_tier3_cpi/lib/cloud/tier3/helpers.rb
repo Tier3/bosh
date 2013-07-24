db09ead6d540b348912d1aa4d6948baeaf3a31c9
Luke Bakken <luke@bowbak.org>
2013-07-23 19:44:38 -0700

    Initial copy of AWS CPI to Tier 3 one.
    Delete unnecessary files.

---
+++ bosh_tier3_cpi/lib/cloud/tier3/helpers.rb
@@ -0,0 +1,19 @@
+# Copyright (c) 2013 Tier 3, Inc.
+
+module Bosh::Tier3Cloud
+
+  module Helpers
+
+    ##
+    # Raises CloudError exception
+    #
+    def cloud_error(message)
+      if @logger
+        @logger.error(message)
+      end
+      raise Bosh::Clouds::CloudError, message
+    end
+
+  end
+end
+
