297c73c69694145c0b28af18ee24781a28ea624d
Eric Lee <saintgimp@hotmail.com>
2013-07-31 15:02:17 -0700

    Improve specs for has_vm? and add specs for create_stemcell and delete_stemcell

--- bosh_tier3_cpi/spec/unit/has_vm_spec.rb
+++ bosh_tier3_cpi/spec/unit/has_vm_spec.rb
@@ -1,23 +1,29 @@
-# Copyright (c) 2009-2013 VMware, Inc.
-# Copyright (c) 2012 Piston Cloud Computing, Inc.
+# Copyright (c) 2013 Tier 3, Inc.
 
 require 'rspec'
 require 'spec_helper'
 require 'rest-client'
 
 describe Bosh::Tier3Cloud::Cloud do
-
-  it "has_vm? returns true if Tier3 vm exists" do
-    cloud = make_cloud do |mock_client|
-      mock_client.stub(:post) {'{"Server": {"ID": 1234} }'}
+  describe '#has_vm?' do
+    it "returns true if Tier3 vm exists" do
+      cloud = make_cloud do |mock_client|
+        mock_client.should_receive(:post)
+        .with('/server/getserver/json', { Name: 'QA1ELETEST01'})
+        .and_return('{"Success": true }')
+      end
+      
+      expect(cloud.has_vm?('QA1ELETEST01')).to be_true
     end
-    
-    expect(cloud.has_vm?('QA1ELETEST01')).to be_true
+
+    it "returns false if Tier3 vm doesn't exist" do
+     cloud = make_cloud do |mock_client|
+        mock_client.should_receive(:post)
+        .with('/server/getserver/json', { Name: 'QA1ELETEST01'})
+        .and_return('{"Success": false }')
+      end
+      
+      expect(cloud.has_vm?('QA1ELETEST01')).to be_false
+   end
   end
-
-  it "has_vm? returns false if Tier3 vm doesn't exists"
-
-  it "has_vm? returns false if Tier3 vm state is :terminated"
-
-  it "has_vm? returns false if Tier3 vm state is :deleted"
 end
