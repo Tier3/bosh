377fec2d1a05e7f57bc061405f6bfe63cf99ebfd
Luke Bakken <luke@bowbak.org>
2013-08-02 09:15:21 -0700

    Fixes #3. Override update_passwords to not set local passwords.

--- bosh_agent/lib/bosh_agent/platform/tier3.rb
+++ bosh_agent/lib/bosh_agent/platform/tier3.rb
@@ -1,11 +1,13 @@
 # Copyright (c) 2013 Tier 3, Inc.
 
 module Bosh::Agent
-
   class Platform::Tier3 < Platform::Ubuntu
     def setup_networking
       # Tier 3 handles its own networking configuration.
     end
-  end
 
+    def update_passwords(settings)
+      # Tier 3 handles its own password updates.
+    end
+  end
 end
